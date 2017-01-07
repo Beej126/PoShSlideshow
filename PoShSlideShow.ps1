5f89991c538ea933e8bdd563689d8a20ffba71db
Beej <beej126@hotmail.com>
2017-01-07 15:27:12 -0800

    minor enhancements
    added U = update folder cache and notifcation prompting to indicate that delay

--- PoShSlideShow.ps1
+++ PoShSlideShow.ps1
@@ -1,17 +1,36 @@
-﻿param(
+param(
   [string]$photoPath = "D:\Photos\_Main_Library",
   [string]$idleTimeout = 2 #minutes
 )
+
+$Error.Clear()
+
+& $PSScriptRoot\Win32.ps1 #bunch of win32 api exports
+
+#save MainWindowHandle so we can hide explicitly here and keep available for showing at end for troubleshooting, IIF PowerShell errors were emitted 
+#nugget: don't use -WindowStyle Hidden on the ps1 shortcut, it prevents retrieval of main MainWindowHandle here...
+$process = Get-Process -Id $pid
+$poShConsoleHwnd = $process.MainWindowHandle
+if ($process.ProcessName -eq "powershell_ise") { $poShConsoleHwnd=0 }
+function showPoShConsole {
+  param([bool]$show = $true)
+  
+  if ($show -and [Win32]::IsWindowVisible($poShConsoleHwnd)) { $show=$false }
+
+  [Win32]::ShowWindowAsync($poShConsoleHwnd, @([Win32]::SW_HIDE, [Win32]::SW_SHOWNORMAL)[$show]) | Out-Null
+  [Win32]::SetForegroundWindow($poShConsoleHwnd) | Out-Null
+}
+
+showPoShConsole $false
+
 
 <#
 Credit: Initial idea and windows forms screen code from here: https://github.com/adamdriscoll/PoshInternals
 #>
 
+# bail out if this same script is already running
 if ((Get-WmiObject Win32_Process -Filter "Name like 'powershell%' AND CommandLine like '%$(split-path $PSCommandPath -leaf)'").Count -gt 1) {
-    #$existingWindow = [win32]::FindWindowByCaption("PoShSlideShow")
-    #[Win32]::SetForegroundWindow($existingWindow)
-    #[Win32]::ShowWindow($existingWindow, 9 <#SW_SHOW#>)
-    exit
+  exit
 }
 
 Add-Type -AssemblyName System.Windows.Forms
@@ -25,10 +44,14 @@ $notifyIcon.Icon = New-Object System.Drawing.Icon "$(Split-Path -parent $PSComma
 #$notifyIcon.Icon = New-Object System.Drawing.Icon ".\icon.ico"
 $notifyIcon.Visible = $true
 
+function saveFolderCache {
+  $folders | export-csv $folderCacheFile -Encoding Unicode #nugget: without -encoding sometimes yielded binary garbage 
+}
+
 function cleanExit {
   $notifyIcon.Visible = $false
   #save the updated folder datestamps to be reloaded next time we start up and keep our randomization fresh
-  $folders | export-csv $folderCacheFile -Encoding Unicode #nugget: without -encoding sometimes yielded binary garbage 
+  saveFolderCache
   [System.Windows.Forms.Application]::Exit()
   exit
 }
@@ -50,28 +73,8 @@ public class DoubleBufferedForm : System.Windows.Forms.Form
     }
 }
 
-public class Win32
-{
-  [DllImport("user32.dll")]
-  [return: MarshalAs(UnmanagedType.Bool)]
-  public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-  [DllImport("user32")]
-  public static extern int SetForegroundWindow(int hwnd);
-
-  [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-  public static extern IntPtr FindWindow(String sClassName, String sWindowCaption);
-
-  [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
-  private static extern IntPtr FindWindowByCaptionInternal(IntPtr ZeroOnly, string sWindowCaption);
-  public static IntPtr FindWindowByCaption(String sWindowCaption)
-  {
-    return FindWindowByCaptionInternal(IntPtr.Zero, sWindowCaption);
-  }
-}
-
 //from: http://stackoverflow.com/questions/15845508/get-idle-time-of-machine
-public static class UserInput {
+public static class IdleHelper {
 
     [DllImport("user32.dll", SetLastError=false)]
     private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
@@ -132,6 +135,10 @@ $script:rotationMap["8"] = [Drawing.RotateFlipType]::Rotate270FlipNone
 $script:filesShown = New-Object collections.arraylist
 $script:rewindIndex = ($script:filesShown.Count - 1)
 
+function isVideo($fileInfoObj) {
+  return $fileInfoObj.Extension -ne ".jpg" -and $fileInfoObj.Extension -ne ".png"
+}
+
 function showImage() {
   $script:img = $null
   if ($script:pictureBox.Image -ne $null) { $script:pictureBox.Image.Dispose() }
@@ -139,12 +146,13 @@ function showImage() {
   $script:pictureBox.Hide()
 
   #video?
-  if ($script:randomFile.Extension -ne ".jpg") {
+  if (isVideo($script:randomFile)) {
     $script:frmFade.Opacity = 0.001
-    start-process -WindowStyle Hidden -wait -filepath vlc -ArgumentList "--fullscreen --video-on-top --play-and-exit `"$($randomFile.FullName)`""
+    start-process -WindowStyle Hidden -wait -filepath "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" -ArgumentList "--fullscreen --video-on-top --play-and-exit `"$($randomFile.FullName)`""
     $script:frmFade.Opacity = 1
     $script:frmFade.BringToFront()
-    #$script:frmFade.Activate()
+    #doesn't give focus to enabled keyboard events - $script:frmFade.Activate()
+    [Win32]::SetFocus($script:frmFade.Handle)
     $script:timerAnimate.Start()
   }
 
@@ -253,7 +261,7 @@ $script:pictureBox.Bounds = $frmImage.Bounds
 $script:idleTimer = New-Object System.Windows.Forms.Timer
 $script:idleTimer.Interval = 30000
 $script:idleTimer.add_Tick({
-  if ([UserInput]::IdleTime.TotalMinutes -gt $script:idleTimeout) {
+  if ([IdleHelper]::IdleTime.TotalMinutes -gt $script:idleTimeout) {
       #pause timer since this is a long retrieval
     $script:idleTimer.Stop()
     #only start if there's nothing currently running on the display
@@ -281,7 +289,8 @@ function ToggleDisplay {
 
     $script:frmFade.Opacity = 1
     $script:frmFade.Show()
-    $script:frmFade.Activate()
+    #doesn't set focus to enable keyboard event handlers - $script:frmFade.Activate()
+    [Win32]::SetFocus($script:frmFade.Handle)
     $script:frmImage.Show()
     $script:frmFade.BringToFront()
 
@@ -293,8 +302,8 @@ function ToggleDisplay {
 }
 
 function pauseAnimation {
-  $script:frmFade.Opacity = 0.05
   $script:timerAnimate.Stop()
+  $script:frmFade.Opacity = 0.05
 }
 
 function reverse {
@@ -324,6 +333,42 @@ function TogglePause {
   if ($script:timerAnimate.Enabled) { pauseAnimation } else {$script:timerAnimate.Start()};
 }
 
+filter IndexPrefix {
+  param([string]$delimiter)
+  $index++
+  "$index$delimiter$($_.replace("$photoPath\", ''))"
+}
+
+function getFolders {
+  return dir -Directory -Recurse -Exclude .* -Path $photoPath | select -ExpandProperty FullName
+}
+
+function createFolderStructs {
+  $input | select @{Name="lastShown"; expression={[datetime]0}}, @{Name="path"; expression={$_}}
+}
+
+function updateFolderCache {
+    showBalloon "Updating folder cache..."
+    $script:existingFolders = $script:folders | select -ExpandProperty path
+    $script:current = getFolders
+    #nugget: "where not exists pattern"
+    $script:newFolders = $script:current |? {$script:existingFolders -notcontains $_} 
+    $script:deleted = $script:folders |? {$script:current -notcontains $_.path} | select -ExpandProperty path #only for display
+    #add newly found folders to the existing collection
+    $script:folders += ($script:newFolders | createFolderStructs)
+    #delete those in collection not found in physical path by creating new list only including those found (since default pipeline behavior creates unmutable arrays)
+    $script:folders = $script:folders |? {$script:current -contains $_.path}
+
+    saveFolderCache
+    $script:pathMenu.Text = pathMenuText
+
+    $wshell.Popup(
+    "Added:`n   $(@("{none}", ($script:newFolders -join "`n   "))[$script:newFolders.Count -gt 0])`n" +`
+    "`nDeleted:`n   $(@("{none}", ($script:deleted -join "`n   "))[$script:deleted.Count -gt 0])`n" +`
+    "`nTotal: " + $script:folders.Count
+    , 0 <#timeout seconds#>, "Folder Cache Updates:", 4096 <#TopMost#> + 64 <#Information icon#>) 
+}
+
 $commands = {
 
     $keyEventArgs = $_
@@ -331,9 +376,11 @@ $commands = {
     $keycode = [string]($keyEventArgs.KeyCode)
     $keycode = $keycode.Substring(0, [math]::Min(6, $keycode.Length))
 
-    if ($keycode -eq "Volume") {return}
+    if ($keycode -eq "Volume" -or $keycode -like "*win*") {return}
 
-    pauseAnimation
+    $wasPaused = -not $script:timerAnimate.Enabled
+
+    pauseAnimation #just makes sense to pause on nearly every keystroke
 
     #debug: $keyEventArgs | Format-List | Out-Host
 
@@ -356,10 +403,30 @@ $commands = {
 
         "Left" { reverse } 
         "Right" { forward }
-        "Space" { TogglePause }
+        "Space" { if ($wasPaused) {$script:timerAnimate.Start()} }
+
+        "D" {
+          $wshell.Popup("last few:`n`n$(($script:filesShown | select -last 20 | IndexPrefix ". ") -join "`n")", 0, "Debug:", 4096 <#TopMost#> + 64 <#Information icon#>)
+          $script:timerAnimate.Start()
+        }
+
+        "U" { updateFolderCache }
+
+        "F" { $script:currentFolder.lastShown = "1/1/1900"; showBalloon "$($script:currentFolder.path) added to favorites"; saveFolderCache }
 
         default {
-            $wshell.Popup("Keycode: $keycode`n`nESC - Exit`nO - Open folder`rC - [C]opy to 'My Pictures'`rM - Open [M]y Pictures folder`rR - Rotate`rLeft Cursor - Previous image`rRight Cursor - Next image`rSpace - Pause", 3 <#timeout seconds#>, "Usage:", 4096 <#TopMost#> + 64 <#Information icon#>) 
+            $wshell.Popup("Keycode: $keycode`n`n" +`
+              "ESC - Exit`nO - Open folder`n" +`
+              "C - [C]opy to 'My Pictures'`n" +`
+              "M - Open [M]y Pictures folder`n" +`
+              "R - Rotate`nLeft Cursor - Previous image`n" +`
+              "Right Cursor - Next image`n" +`
+              "Space - Pause`n" +`
+              "D - Debug - list last few showings`n" +`
+              "U - update folder cache with additions & deletions - preserves existing timestamps`n" +`
+              "F - mark folder as favorite"
+              , 3 <#timeout seconds#>, "Usage:", 4096 <#TopMost#> + 64 <#Information icon#>) 
+            
             $script:timerAnimate.Start()
 		}
     }
@@ -374,7 +441,7 @@ if (!(test-path $folderCacheFile)) {
   showBalloon "Creating folder cache: $folderCacheFile"
   try {
     #nugget: -ExpandProperty prevents ellipsis on long strings
-    $folders = dir -Directory -Recurse -Exclude .* -Path $photoPath | select @{Name="lastShown"; expression={[datetime]0}}, @{Name="path"; expression={$_.FullName}}
+    $newfolders = getFolders | createFolderStructs
     $folders | Export-Csv $folderCacheFile
   }
   catch {
@@ -409,18 +476,33 @@ $script:timerAnimate.add_Tick({
               |_| |_|\___|_|  \___| |___/  \__|_| |_|\___| |_.__/ \___|\___|_|    
             ######################################################################>
 
-            #get next random folder... that we haven't seen for XX days
-            $skipCount = 0
-            do { $folder = $folders | random; $skipCount++ } until ($folder.lastShown -lt [DateTime]::UtcNow.AddMonths(-1) -or $skipCount -eq $folders.Count )
-            $folder.lastShown = [DateTime]::UtcNow
+            do {
 
-            #get next randome file
-            #nugget: http://stackoverflow.com/questions/790796/confused-with-include-parameter-of-the-get-childitem-cmdlet
-            #there's an interesting interplay between -filter, -include and -recurse...
-            #-filter only applies to a single extension, so we need to use -include (less performant since it scans the whole folder but we don't expect that many files per folder)
-            #the wildcard on end of path allows the -include to apply to the contents... otherwise the -include operates on the path vs the contents
-            #-recurse would basically work without the wildcard tacked on to the path... but then it digs into subfolders undesirably for this use case
-            $script:randomFile = gci -Path "$($folder.path)\*" -File -Include @("*.jpg", "*.mp4", "*.mov", "*.avi") | random 
+                #get next random folder... that we haven't seen for XX days... 
+                $lookBackDays = -12
+                do {
+                  $lookBackDays += 2 # progressively relaxing freshness if none found
+                  $freshies = $folders | where { $_.lastShown -lt [DateTime]::UtcNow.AddDays($lookBackDays) }
+                }
+                until ($freshies.Count -gt 0 -or $lookBackDays -eq 0)
+
+                $script:currentFolder = @($folders, $freshies)[$freshies.Count -gt 0] | random #should never happen that max lastShown is somehow greater than "now" but just in case, bail out and pull from the whole list
+                if ($script:currentFolder.lastShown -eq "1/1/1900") {} #skip update if this is a favorite, favorite flagged by this special datestamp
+                #elseif ($script:currentFolder.path -like "*unfiled*") { $script:currentFolder.lastShown = "1/1/1901" -as [datetime] } #any folder with "unfiled" in the name, never gets datestamped, always "fresh" in the rotation
+                else { $script:currentFolder.lastShown = [DateTime]::UtcNow }
+
+                #get next randome file
+                #nugget: http://stackoverflow.com/questions/790796/confused-with-include-parameter-of-the-get-childitem-cmdlet
+                #there's an interesting interplay between -filter, -include and -recurse...
+                #-filter only applies to a single extension, so we need to use -include (less performant since it scans the whole folder but we don't expect that many files per folder)
+                #the wildcard on end of path allows the -include to apply to the contents... otherwise the -include operates on the path vs the contents
+                #-recurse would basically work without the wildcard tacked on to the path... but then it digs into subfolders undesirably for this use case
+                $showFiles = gci -Path "$($script:currentFolder.path)\*" -File -Include @("*.jpg", "*.mp4", "*.mov", "*.avi")
+            }
+            until ($showFiles.count -gt 0) #account for folders without any viewable files
+
+            $script:randomFile = $showFiles | random
+
             $script:filesShown.Add($script:randomFile.FullName)
             $script:rewindIndex = ($script:filesShown.Count - 1)
             #$script:debugLabel.Text = $script:rewindIndex
@@ -465,10 +547,14 @@ $notifyIcon.add_MouseDown( {
   [System.Windows.Forms.NotifyIcon].GetMethod("ShowContextMenu", [System.Reflection.BindingFlags] "NonPublic, Instance").Invoke($script:notifyIcon, $null)
 })
 
+function pathMenuText {
+  return "Path: $script:photoPath ({0} folders)" -f $script:folders.Count
+}
+
 $contextMenu = New-Object System.Windows.Forms.ContextMenuStrip
 $contextMenu.ShowImageMargin = $false
 $notifyIcon.ContextMenuStrip = $contextMenu
-$contextMenu.Items.Add( "Path: $photoPath", $null, $null ) | Out-Null
+$script:pathMenu = $contextMenu.Items.Add( $(pathMenuText), $null, { updateFolderCache } ) 
 $contextMenu.Items.Add( "E&xit", $null, { cleanExit } ) | Out-Null
 
 $enableMenuItem = $contextMenu.Items.Add( "Disable Idle", $null, {
@@ -480,4 +566,4 @@ $playMenu = $contextMenu.Items.Add( "Play", $null, { ToggleDisplay } )
 
 $script:idleTimer.Start()
 [System.Windows.Forms.Application]::Run()
-if ($Error -ne $null) { showBalloon $Error "error" }
+if ($Error -and $poShConsoleHwnd -ne 0) { showPoShConsole; pause }
